2daabcc12b1e745bcb9a2bfe4994e231f5df8e55
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-11-05 15:13:10 -0500

    initial commit

--- database.sql
+++ database.sql
@@ -3,6 +3,9 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+--create a database named: group_spike
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
