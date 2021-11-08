c71d91bf4222985a9d05dc9f9eb6671156b55721
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-11-07 20:48:47 -0500

    created function sendMail.js with module exports

---
+++ server/modules/sendMail.js
@@ -0,0 +1,30 @@
+//function to send confirmation email
+//takes an email and random string
+const sendMail = (email, ranStr) => {
+    const Transport = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: "anthonyvlynch5@gmail.com",
+            pass: "Neep.gal856"
+        }
+    });
+    let mailOptions;
+    let sender = "Anthony Lynch";
+    mailOptions = {
+        from: sender,
+        to: email,
+        subject: "Email confirmation",
+        html: `Press <a href=http://localhost:3000/api/user/verify/${ranStr}> here </a> to verify your email!`,
+    };
+    Transport.sendMail(mailOptions, function(error, response) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email verification sent");
+        }
+    });
+}
+
+module.exports = {
+    sendMail,
+};
